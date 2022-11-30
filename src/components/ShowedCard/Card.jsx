693201f773b041be309ee0c0462d60d4e3723250
Carlos isen <amra_akyra@hotmail.com>
2022-11-30 19:52:56 +0000

    prueba_popup

---
+++ src/components/ShowedCard/Card.jsx
@@ -0,0 +1,30 @@
+import AsideCard from "./AsideCard";
+import BodyCard from "./BodyCard";
+const Card = (props) => {
+
+   
+    let current = props.currentCards?.filter(a => a.id === props.currentCard)
+
+return(
+    <div className="card__total">
+        <div className="card__title">
+            <h1>{current[0].name}</h1>
+            <p className="XButton" onClick={()=>props.showCard(null)}>X</p>
+        </div>
+        <div className="card__main">
+            <div className="card__body">
+                <BodyCard payload={current[0]}/>
+            </div>
+            <div className="card__aside">
+                <AsideCard/>
+            </div>
+        </div>
+
+    </div>
+)
+
+
+
+
+}
+export default Card
