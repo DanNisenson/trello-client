6ede28c31207f53443d07624d4f0d2d80e0fae2c
Carlos isen <amra_akyra@hotmail.com>
2022-12-01 09:44:29 +0000

    update_popup

---
+++ src/components/ShowedCard/AsideCard.jsx
@@ -0,0 +1,17 @@
+
+
+
+const AsideCard= ()=>{
+
+    return(
+        <>
+            <p className="card__aside__options">Checkpoints</p>
+            <p className="card__aside__options">Fechas</p>
+            <p className="card__aside__options">Archivar</p>
+
+        </>
+    )
+
+}
+
+export default AsideCard
--- src/components/SingleList.jsx
+++ src/components/SingleList.jsx
@@ -1,17 +1,30 @@
-//import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 //import { useAppContext } from "../context/keys";
 //import axios from "axios";
 import Cards from "./Cards";
+import Card from "./ShowedCard/Card";
 import "../assets/css/Lists.css";
+import "../assets/css/Card/Card.css"
 
 const SingleList = (props) => {
+    //state and function to show a card
+    const [currentCard, SetcurrentCard] = useState(null)
+    const showCard = (i) => {
+        SetcurrentCard(i);
+    }
+
     //name should become an input on click, for update
     return (
-        <div className="lists__list">
-            {props.name}
-            <button>DELETE LIST</button>
-            <Cards currentCards={props.currentCards} listId={props.id} />
-        </div>
+        <>
+            <div className="lists__list">
+                {props.name}
+                <button>DELETE LIST</button>
+                <Cards currentCards={props.currentCards} listId={props.id} showCard={(i) => showCard(i)} />
+            </div>
+            <div className="card" style={{ display: currentCard ? "block" : "none" }}>
+                {currentCard ? <Card currentCards={props.currentCards} currentCard={currentCard} showCard={(i) => showCard(i)} /> : null}
+            </div>
+        </>
     );
 }
 
