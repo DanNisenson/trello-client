4c9577b9c78f677f22a265be25dc704d51846592
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2022-12-05 12:29:36 +0100

    Creating Authorize Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import {useState} from "react";
 import { AppWrapper } from "./context/keys.js";
 import Board from "./components/Board";
+import Authorize from "./components/Authorize";
 import "./assets/css/App.css";
 
 function App() {
+    const [isAuthorized, setIsAuthorized] = useState(false);
   return (
     // context variables
     <AppWrapper>
       <div className="App">
-        {/* main view */}
-        <Board />
+        {isAuthorized ? <Board /> : <Authorize isAuthorized={isAuthorized} setIsAuthorized={setIsAuthorized} />}
       </div>
     </AppWrapper>
   );
---
+++ src/components/Authorize.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import { useAppContext } from "../context/keys";
+import "../assets/css/Authorize.css";
+
+const Authorize = props => {
+    const context =useAppContext();
+
+    return (
+        <div className="authorize">
+            <h1>Welcome to our Trello client!</h1>
+            <div className="authorize__text">
+                <h3>In order to use it, we will require you to first authorize the app.</h3>
+                <p>Please click the button below to be redirected to the Trello authorization page.</p>
+                <p>This Application will request the following access: <span className="authorize__text-bold">Read & Write to your boards for 1 hour.</span></p>
+                <p>Once you authorize the Application, you will be redirected back here.</p>
+            </div>
+            <button className="authorize__button" onClick={() => props.setIsAuthorized(!props.isAuthorized)}>Authorize</button>
+        </div>
+    );
+}
+
+export default Authorize;
