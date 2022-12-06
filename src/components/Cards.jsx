0bbdc0a8df150a6e5eb8e8331d2ebc77cc9d9324
DanNisenson <danpablo1@gmail.com>
2022-12-06 16:55:44 +0100

    added card edit mode and styling

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,17 +3,14 @@ import "../assets/css/Cards.css";
 import SingleCard from "./SingleCard";
 
 const Cards = (props) => {
-  const [modal, setModal] = useState(false)
-  console.log(modal)
 
   
   return (
     <div className="cards" >
-      {modal ? <div className="cards__modal-bkground"></div> : null}
       {/* iterate and print all cards on board */}
       {props.listCards.map((card) => {
         return (
-          <SingleCard id={card.id} name={card.name} listCards={props.listCards} setListCards={props.setListCards} modal={modal} setModal={setModal} showCard={props.showCard} key={card.id} />
+          <SingleCard id={card.id} name={card.name} listCards={props.listCards} setListCards={props.setListCards} showCard={props.showCard} key={card.id} />
         );
       })}
     </div>
