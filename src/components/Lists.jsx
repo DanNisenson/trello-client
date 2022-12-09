aa4d91acdeba51af49bc79ae6c1c93947b897dbe
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2022-12-09 20:03:08 +0100

    Creating boardAPI class

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -1,35 +1,37 @@
 import { useState, useEffect } from "react";
 import { useAppContext } from "../context/context";
-import axios from "axios";
+import listsAPI from "../services/listsAPI";
 import SingleList from "./SingleList";
 import CreateList from "./CreateList";
 import "../assets/css/Lists.css";
 
 const Lists = (props) => {
     const context = useAppContext();
-    const [currentLists, setCurrentLists] = useState([]);
+    // const [currentLists, setCurrentLists] = useState([]);
 
     // get board's lists on props(board selection) change.
     useEffect(() => {
         const getLists = async () => {
-            const resp = await axios.get(
-                `https://api.trello.com/1/boards/${props.boardId}/lists?&key=${context.keys.apiKey}&token=${context.keys.token}`
-            );
-            setCurrentLists(resp.data);
+            try {
+                const resp = await listsAPI.getLists(context.keys.apiKey, context.keys.token, props.boardId);
+                console.log(resp);
+                context.setLists(resp.data);
+            } catch (error) {
+                console.log('can\'t get lists.')                
+            }
         }
         getLists();
-    }, [props]);
+    }, [props.boardId]);
 
     return (
         // iterate lists on selected board and render each, passing only the filtered cards that belong to each list
         <div className="lists">
-            {currentLists
+            {context.lists
                 .map(list => 
-                    <SingleList key={list.id} list={list} currentLists={currentLists} setCurrentLists={setCurrentLists} />
+                    <SingleList key={list.id} list={list} currentLists={context.lists} setCurrentLists={context.setLists} />
             )}
             <CreateList boardId={props.boardId}
-                currentLists={currentLists}
-                setCurrentLists={setCurrentLists} />
+                currentLists={context.lists} setCurrentLists={context.setLists} />
         </div>
     );
 };
